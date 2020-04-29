9949e6c185ab5fc3d13b9cf12c4c19d30efbcbb2
Nishanth Singh Gurung <nishanthsinghgurung@OVO4680MB.local>
2020-04-29 17:19:35 +0100

    stream client implementation

---
+++ src/components/streams/StreamDelete.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Modal from '../Modal';
+import history from '../../history';
+import { fetchStream } from '../../actions';
+
+class StreamDelete extends React.Component {
+
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id);
+    }
+
+    renderActions() {
+        return (
+            <React.Fragment>
+                <button className="ui button negative">Delete</button>
+                <button className="ui button">Cancel</button>
+            </React.Fragment>
+        );
+    }
+    
+    render() {
+        return (
+            <div>
+                StreamDelete
+                <Modal 
+                    title="Delete Stream" 
+                    content="Are you sure you want to delete the stream?"
+                    actions={this.renderActions()}
+                    onDismiss={() => history.push('/')}
+                ></Modal>
+            </div>
+        );
+    }
+    
+}
+
+export default connect(null, { fetchStream })(StreamDelete);
